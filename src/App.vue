1bc73e7ca9fdd1f876e48a4c21477a0a5da5fd1e
Mariya <bessarabenko.m.i20@gmail.com>
2021-10-21 00:11:52 +0300

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+  <v-navigation-drawer app></v-navigation-drawer> 
+  <v-app-bar app dark color="primary">
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+       <v-btn  text><v-icon left>mdi-cake-variant</v-icon>Link One</v-btn> 
+       <v-btn  text>Link Two</v-btn> 
+       <v-btn  text>Link Three</v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+  <v-content>
+    <router-view></router-view> 
+  </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<i class="fi fi-rr-fox"></i>
