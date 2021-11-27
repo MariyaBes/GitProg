35c75565af8317e77ada600bbc09836fc2fa4cfd
MariyaBes <bessarabenko.m.i20@gmail.com>
2021-11-27 15:15:03 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@
           <v-list-item
           v-for="link in links" 
           :key="link.title"
+          :to="link.url"
           >
             <v-list-item-icon>
               <v-icon>{{ link.icon }}</v-icon>
@@ -36,7 +37,8 @@
     <v-toolbar-items class="hidden-sm-and-down">
        <v-btn  
        v-for="link in links" 
-       :key="link.title" 
+       :key="link.title"
+       :to="link.url" 
        text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn> 
     </v-toolbar-items>
   </v-app-bar>
