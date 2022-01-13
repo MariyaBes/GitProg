dca4d86fbd60ba80d00627ea9eaa541fad4b23c7
MariyaBes <bessarabenko.m.i20@gmail.com>
2022-01-13 18:02:59 +0300

    registerUser

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router/index'
 import store from './store'
+import fb from 'firebase'
 
 Vue.use(Router)
 Vue.config.productionTip = false
@@ -12,5 +13,19 @@ new Vue({
   vuetify,
   render: h => h(App),
   router:router,
-  store
+  store,
+  created(){
+    const firebaseConfig = {
+      apiKey: "AIzaSyAbRDrYIT1sGu8_4FD-xhQr1ZNRZv9bAk4",
+      authDomain: "mariya-785cb.firebaseapp.com",
+      projectId: "mariya-785cb",
+      storageBucket: "mariya-785cb.appspot.com",
+      messagingSenderId: "790408619912",
+      appId: "1:790408619912:web:593012a40f856db5b9fc3d",
+      measurementId: "G-DNT7C8EMRV"
+    };
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
 }).$mount('#app')
